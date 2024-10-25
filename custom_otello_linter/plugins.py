3d14fe378be38d7adc74dfde214ea47a7f4a8e1d
iri6e4ko <iri6e4ko@gmail.com>
2024-10-25 12:11:27 +0700

    update version

--- custom_otello_linter/plugins.py
+++ custom_otello_linter/plugins.py
@@ -35,7 +35,7 @@ class PluginWithFilename(Plugin):
 
 class OtelloQAStylePlugin(PluginWithFilename):
     name = 'custom_otello_linter'
-    version = '1.0.0'
+    version = '1.0.1'
     visitors = [
         ScenarioVisitor,
     ]
