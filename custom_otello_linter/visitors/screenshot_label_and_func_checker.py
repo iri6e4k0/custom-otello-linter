962fb2925975eb6e153b35cf52437a98235c851f
a.pozdeeva <a.pozdeeva@2gis.ru>
2025-02-05 16:59:22 +0200

    micro changes

--- custom_otello_linter/visitors/screenshot_label_and_func_checker.py
+++ custom_otello_linter/visitors/screenshot_label_and_func_checker.py
@@ -27,28 +27,32 @@ class ScreenshotsLabelAndFuncChecker(StepsChecker):
         has_screenshot_label = False
         has_screenshot_func = False
 
+        # Проверяем, есть ли в списке лейблов SCREENSHOTS
         if allure_decorator:
             allure_tags = self.get_allure_tag_names(allure_decorator)
             has_screenshot_label = "SCREENSHOTS" in allure_tags
 
+        # Проверяем наличие вызова функции make_screenshot_for_comparison
         for step in context.steps:
             for stmt in step.body:
                 for node in ast.walk(stmt):
                     if isinstance(node, ast.Call):
                         func_name = get_full_func_name(node.func)
-                        print(f"Found function call: {func_name}")
                         if func_name.endswith("make_screenshot_for_comparison"):
                             has_screenshot_func = True
                             break
 
+        # Если в тесте есть лейбл, но нет вызова функции – добавляем ошибку
         if has_screenshot_label and not has_screenshot_func:
             errors.append(MissingMakeScreenshotFuncCallError(
                 lineno=context.scenario_node.lineno,
                 col_offset=context.scenario_node.col_offset))
 
+        # Если в тесте есть вызов функции, но нет лейбла – добавляем ошибку
         if has_screenshot_func and not has_screenshot_label:
             errors.append(MissingAllureScreenshotsLabelError(
                 lineno=allure_decorator.lineno,
                 col_offset=allure_decorator.col_offset))
 
+        # Возвращаем собранные ошибки после завершения всех шагов
         return errors
